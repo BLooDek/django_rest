d98a863390fd777058a69ef49e9af5a9272fcdbe
BLooDek <pawelmichalak98@gmail.com>
2022-11-06 23:34:08 +0100

    added get all users

--- user_api/urls.py
+++ user_api/urls.py
@@ -4,5 +4,6 @@ from . import  views
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('register/', include('dj_rest_auth.registration.urls')),
-    path('valid/<str:email>', views.user_exist)
+    path('valid/<str:email>', views.user_exist),
+    path('all', views.users_get_all)
 ]
--- user_api/views.py
+++ user_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.response import Response
 
 from user_api.models import User
+from user_api.serializers import UserSerializer
 
 
 @api_view(['GET'])
@@ -16,3 +17,9 @@ def user_exist(request, email):
         # return Response(status=status.HTTP_418_IM_A_TEAPOT)
     return Response({'detail': 'false'}, status=status.HTTP_200_OK)
 
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+def users_get_all(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
