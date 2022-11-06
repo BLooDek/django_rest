947b618fd4c395e1a5d0f50430a2a567365dddc2
BLooDek <pawelmichalak98@gmail.com>
2022-11-06 12:16:36 +0100

    validator if user exist

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
+from . import  views
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('register/', include('dj_rest_auth.registration.urls')),
+    path('valid/<str:email>', views.user_exist)
 ]
--- user_api/views.py
+++ user_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import permissions, status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.response import Response
+
+from user_api.models import User
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def user_exist(request, email):
+    if User.objects.filter(email=email).exists():
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+    return Response(status=status.HTTP_200_OK)
+
